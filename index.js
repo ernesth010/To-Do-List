7c12ab20c8e5a9e40ff6d158c087e630bef40003
Muigai Ernest <ernestmbogo7@gmail.com>
2025-09-19 15:16:55 +0300

    progress

--- index.js
+++ index.js
@@ -19,4 +19,58 @@ taskInput.addEventListener("keydown", (e) =>{
 });
 clearCompletedBtn.addEventListener("click",clearCompleted)
 
-// function clearCompleted()
+function addTodo(text) {
+    if (text.trim() === "") return;
+    const todo = {
+        id: Date.now(),
+        text,
+        completed: false,
+    }
+    todos.push(todo)
+    saveTodos()
+}
+function saveTodos(){
+    localStorage.setItem("todos", JSON.stringify(todos));
+    updateItemsCount()
+    checkEmptyState()
+}
+function updateItemsCount() {
+const uncompletedTodos = todo.filter(todo => !todo.completed);
+itemsLeft.textContent = `${uncompletedTodos.length} item${
+uncompletedTodos.length !== 1 ? "s" : ""  
+} left`;
+}
+function checkEmptyState() {
+const filteredTodos = filterTodos(currentFilter);
+if (filteredTodos.length === 0) emptyState.classList.remove("hidden");
+else emptyState.classList.add("hidden");
+}
+function filterTodos(filter) {
+    switch (filter) {
+        case 'active':
+            return todos.filter(todo => !todo.completed);
+        case 'completed':
+            return todos.filter(todo => todo.completed);
+        default:
+            return todos;
+    }
+}
+function renderTodos() {
+    todosList.innerHTML = "";
+    const filteredTodos = filterTodos(currentFilter);
+    filteredTodos.forEach(todo => {
+        const todoItem = document.createElement("li");
+        todoItem.classList.add = ("todo-item")
+        if(todo.completed) todoItem.classList.add("completed");
+        const checkboxContainer = document.createElement("label");
+        checkboxContainer.classList.add("checkbox-container");
+        const checkBox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add = ("todo-checkbox");
+        checkbox.checked = todo.completed;
+        checkBox.addEventListener("change", () => toggleTodoCompletion(todo.id));
+    })
+    } 
+
+
+function clearCompleted() {}   
